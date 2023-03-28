b6ecd0311d78f2386619f24ebe7482348fa811b7
ddeoyoung <ksysnake3690@gmail.com>
2023-03-28 20:06:52 +0900

    012 ~ 019 업데이트

---
+++ CppStudy/017_Printf/017_Printf.cpp
@@ -0,0 +1,41 @@
+﻿// 017_Printf.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+
+    // 콘솔창에 글자를 띄운다.
+    // printf_s("aaaaa");
+
+
+    //char Arr[5] = "abc";
+    //printf_s(Arr);
+
+    1;
+    '1';
+    '2';
+    '3';
+    // 문자 
+
+    // 숫자 1
+    // "12345" 문자열 상수 표현식
+    // 문자열 상수 표현식을 사용하면 무조건 마지막에 컴파일러가 알아서
+    // 0을 붙이기 때문에 
+    char Arr[6] = "12345"/*0*/;
+    // char Arr[5] = {'1', '2','3','4',0/*'5'*/};
+
+    // 자료형이 다르기 때문에 배열에 들어가지 못한다.
+    // char Value1 = Arr;
+
+    // 제로베이스 : 배열의 개수를 샐때 0부터 세는 것
+    char Value1 = Arr[0]; // 1
+    char Value2 = Arr[1]; // 2
+    char Value3 = Arr[2]; // 3
+    char Value4 = Arr[3]; // 4
+    char Value5 = Arr[4]; // 5
+
+    printf_s(Arr);
+
+}
---
+++ CppStudy/018_Const01/018_Const01.cpp
@@ -0,0 +1,34 @@
+﻿// 018_Const01.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+
+// 함수의 스택에서의 메모리 크기는 컴파일시 고정되어야 한다.
+// ex) c++의 함수의 실행흐름을 만들기 위한 스택크기는
+//     exe파일에 이미 다 정의되어 있어야 한다.
+
+int main()
+{
+    "Testchar"; // const char[9]
+
+    int Value = 20;
+
+    const bool cb = 20;
+
+    const char cc = 20;
+
+    // 한번더 보게 될것
+    const int ci = 20;
+
+
+    // 특정 자료형앞에 const 키워드를 붙이면
+    // 그 메모리를 비트적 상수화 시킨다.
+
+
+    // int ArrSize = 30;
+    // int Value[ArrSize] = {}; 불가능 -> 메모리 크기가 변수이기 때문
+
+    const int ArrSize = 30;
+    int Value[ArrSize] = {}; // 가능
+}
