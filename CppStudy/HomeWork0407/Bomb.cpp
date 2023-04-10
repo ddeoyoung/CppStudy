5ad2246affa05d41d439f72788b871ab3627f1f9
ddeoyoung <ksysnake3690@gmail.com>
2023-04-10 22:40:29 +0900

    HomeWork0407 추가

---
+++ CppStudy/HomeWork0407/Bomb.cpp
@@ -0,0 +1,32 @@
+#include "Bomb.h"
+
+Bomb::Bomb()
+{
+}
+
+Bomb::~Bomb()
+{
+}
+
+void Bomb::Init()
+{
+	RenderChar = '@';
+}
+
+// explogen
+
+void Bomb::Update()
+{
+	ConsoleGameObject::Update();
+
+	if (0 >= --BoomCount)
+	{
+		Off();
+	}
+}
+
+
+void Bomb::Render()
+{
+	ConsoleGameObject::Render();
+}
