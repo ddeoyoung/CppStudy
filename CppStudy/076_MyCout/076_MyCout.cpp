e349e4c9eea051b4308754705e71460ed654b17e
ddeoyoung <ksysnake3690@gmail.com>
2023-04-14 23:59:15 +0900

    HomeWork0414 추가

---
+++ CppStudy/076_MyCout/076_MyCout.cpp
@@ -0,0 +1,30 @@
+﻿// 076_MyCout.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+
+class MyCout
+{
+public:
+    void operator<<(const char* _Test)
+    {
+        printf_s(_Test);
+    }
+
+private:
+};
+
+
+namespace std
+{
+    MyCout MCout;
+}
+
+int main()
+{
+
+    std::MCout << "Hello World!\n";
+
+    std::cout << "Hello World!\n";
+}
---
+++ CppStudy/GameEngineBase/GameEngineRandom.h
@@ -0,0 +1,22 @@
+#pragma once
+
+// Ό³Έν :
+class GameEngineRandom
+{
+public:
+	// constrcuter destructer
+	GameEngineRandom();
+	~GameEngineRandom();
+
+	// delete Function
+	GameEngineRandom(const GameEngineRandom& _Other) = delete;
+	GameEngineRandom(GameEngineRandom&& _Other) noexcept = delete;
+	GameEngineRandom& operator=(const GameEngineRandom& _Other) = delete;
+	GameEngineRandom& operator=(GameEngineRandom&& _Other) noexcept = delete;
+
+protected:
+
+private:
+
+};
+
