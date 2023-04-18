f1cc7c18cdcfeca1dbccfa62fa7293ab0e4552f0
ddeoyoung <ksysnake3690@gmail.com>
2023-04-19 00:14:26 +0900

    0418 과제 수정

--- CppStudy/HomeWork0414/Parts.cpp
+++ CppStudy/HomeWork0414/Parts.cpp
@@ -36,9 +36,14 @@ void Parts::NextMove()
 		return;
 	}
 
+	Next->SetPos(GetPrevPos());
 
+	// 뱀의 이동방향에 따른 몸통 모양 바꾸기
+	// ▲ ▼ ◀ ▶
 	// 다음 위치 - 현재 위치
-	int2 CurPos = GetPrevPos();
+
+
+	int2 CurPos = GetPos();
 	int2 NextPos = Next->GetPos();
 
 	int2 dir = { CurPos.X - NextPos.X, CurPos.Y - NextPos.Y };
@@ -59,25 +64,6 @@ void Parts::NextMove()
 	{
 		Next->RenderChar = L'▼';
 	}
-
-
-
-
-	Next->SetPos(GetPrevPos());
-
-	// ▲ ▼ ◀ ▶
-	// 다음 위치 - 현재 위치
-
-	/*int2 CurPos = GetPrevPos();
-	int2 NextPos = GetPos();
-
-	int2 Dir = NextPos - CurPos;
-
-	if (Dir == int2::Right)
-	{
-		RenderChar = L'▶';
-	}*/
-
 
 	return Next->NextMove();
 
