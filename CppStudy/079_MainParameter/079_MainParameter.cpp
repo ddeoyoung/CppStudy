638b90cb36ac677e052b0a1ca412db9a91bd4528
ddeoyoung <ksysnake3690@gmail.com>
2023-04-24 16:42:44 +0900

    MainParameter, TypeDefEx 추가

---
+++ CppStudy/079_MainParameter/079_MainParameter.cpp
@@ -0,0 +1,25 @@
+﻿// 079_MainParameter.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main(int _Count, const char** _InitText)
+{
+
+	// CWindow NewWindow = new CWindow();
+	// NewWindow.Open();
+	// 
+	// 
+
+	// 내 프로세스가 다른 프로세스를 실행할수도 있습니다.
+	// 내가 실행하는 곳에 따라서 위치가 달라진다.
+	int Result = system("D:\\Project\\AR47\\CPlusPlus\\x64\\Debug\\078_BitTest.exe");
+
+	for (size_t i = 0; i < _Count; i++)
+	{
+		// char*
+		std::cout << _InitText[i] << std::endl;
+	}
+
+    return 1; // 메인에서 왜 리턴할 필요가 있을까?
+}
---
+++ CppStudy/080_TypeDefEx/080_TypeDefEx.cpp
@@ -0,0 +1,17 @@
+﻿// 080_TypeDefEx.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+typedef int MYINT, * MYTEST;
+
+
+int main()
+{
+    int* Ptr0 = nullptr;
+    MYTEST Ptr1 = nullptr;
+
+    // wchar_t* LPWSTR;
+
+    std::cout << "Hello World!\n";
+}
