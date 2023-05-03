0a1eeeddf77d7e8eaeda06fb381b40a5113a0e48
ddeoyoung <ksysnake3690@gmail.com>
2023-05-03 20:56:19 +0900

    Union 추가

---
+++ CppStudy/084_DefaultParameter/084_DefaultParameter.cpp
@@ -0,0 +1,38 @@
+﻿// 084_DefaultParameter.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+void Function(int _Value = 50)
+{
+    std::cout << _Value << std::endl;
+}
+
+
+void Function(int _Value, int _Test = 50)
+{
+    std::cout << _Value << std::endl;
+}
+
+
+
+// 디폴트 인자는 무조건 인자의 가장 오른쪽부터 만들수 있다.
+//void Function0(int _Value = 50, int _Test)
+//{
+//    std::cout << _Value << std::endl;
+//}
+int main()
+{
+    Function(10, 20);
+
+    Function();
+    Function();
+    Function();
+    Function();
+    Function();
+    Function();
+    Function();
+    // Function(20);
+
+    std::cout << "Hello World!\n";
+}
---
+++ CppStudy/085_Float/085_Float.cpp
@@ -0,0 +1,24 @@
+﻿// 085_Float.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+    // int로 바꾸면? 0
+    int Value = 1.1f;
+    float Valuef = 1.00001f;
+    Valuef += 1.00001f;
+
+    if (Valuef == 2.00002f)
+    {
+        int a = 0;
+    }
+
+    for (size_t i = 0; true; i++)
+    {
+        std::cout << "fashdjklfashdjklfasdhjklfhasdjklfhajsdklfhjkladsfhjklasdhfjklads" << std::endl;
+    }
+
+
+}
